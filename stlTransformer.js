02943fb8ed6cdceff28241213dcfe4b0c7c90b4f
blue <blueeagletechnologies@gmail.com>
2016-02-26 11:45:58 -0800

    clean up

--- stlTransformer.js
+++ stlTransformer.js
@@ -15,46 +15,13 @@ module.exports = {
         var ON_DOCUMENT_SCANNED = "OnNewDocumentScanned";
         var IOM_INSTALATION_DIR = "C:\\VERIFIERv2-IOM\\Acquisition\\Temp\\";
 
-        var FINGERPRINT_URL = "fingerprintUrl";
-        var PHOTO_URL = "photoUrl";
-        var DOC_TYPE = "docType";
-        var GAP = 1;
-
         if (event.indexOf(ON_FINGERPRINT_SCANNED) > -1 || event.indexOf(ON_PHOTO_TAKEN) > -1
             || event.indexOf(ON_DOCUMENT_SCANNED) > -1){
             return getXMLResult(rawMessage);
         }
-        //else if (event.indexOf(DOC_TYPE) > -1){
-        //    GAP=8;
-        //    var transformedStr = "[\"" + ON_DOCUMENT_SCANNED + "\",\"" + getXMLResult(rawMessage) + "\"]";
-        //    var newBuffer3 = createNewBuffer(extractedModifyHeader(rawMessage), transformedStr);
-        //    return newBuffer3;
+        //else if (){
         //} else {
-        //    return data;
         //}
-
-        function createNewBuffer(header, transformedStr) {
-            var modifiedBuffer = new Buffer(header + "42" + transformedStr, "binary");
-            var upperThreshold = modifiedBuffer.length - GAP;
-            var numberArray = upperThreshold.toString().split('');
-
-            for (var i = 0; i < numberArray.length; i++) {
-                modifiedBuffer[i + 1] = numberArray[i];
-            }
-            modifiedBuffer[numberArray.length + 1] = 255;
-
-            return modifiedBuffer;
-        }
-        function updateHeader(header) {
-            var newCountStr = transformedStr.length + header.length + 1 + '';
-            var existingCountStr = rawMessage.length + '';
-
-            if (newCountStr.length - existingCountStr.length > 0) {
-                header = header + ' ';
-            }
-
-            return header;
-        }
 
         function convertResultToXML(result) {
             var options = {
@@ -66,14 +33,8 @@ module.exports = {
             var xmlResult = js2xmlparser("result", result, options);
             xmlResult = xmlResult.replace(/(\n)/gm, "");
             xmlResult = xmlResult.replace(/("|&quot;)/gm, "\\\"");
-            //xmlResult = replaceImageURL(xmlResult); // replace image url with base64 image
 
             return xmlResult;
-        }
-
-        function replaceImageURL(xmlResult) {
-
-            console.log(result);
         }
 
         function extractedResult(rawMessage) {
@@ -82,10 +43,8 @@ module.exports = {
                 "message": "",
                 "sensorData": []
             };
-            //var rawResult = JSON.parse(rawMessage.substring(rawMessage.indexOf("["), rawMessage.length));
 
             result.status = 'success';
-
             result.message = rawMessage;
 
             var imagePathArray = result.message.match(/(C:.*?\.(jpg|bmp))/img);
@@ -96,25 +55,16 @@ module.exports = {
 
                     var base64 = base64Encode(imageURL);
                     result.sensorData.push({'imageURL' : imageURL, 'base64': base64});
-                    //result.sensorData.push({'imageURL' : imageURL, 'base64': 'base64'});
                 }
             }
 
             return result;
         }
 
-
         function getXMLResult(rawMessage) {
             var result = extractedResult(rawMessage);
             var xmlResult = convertResultToXML(result);
             return xmlResult;
-        }
-
-        function extractedModifyHeader(rawMessage) {
-            var breakPoint = rawMessage.indexOf("42[");
-            var header = rawMessage.substring(0, breakPoint);
-            header = updateHeader(header);
-            return header;
         }
 
         function base64Encode(file) {
@@ -123,15 +73,5 @@ module.exports = {
             // convert data to base64 encoded string
             return new Buffer(bitmap).toString('base64');
         }
-
-
-    },
-
-    /**
-     *
-     */
-    unescape: function(html) {
-        return String(html)
-            .replace(/&gt;/g, '>');
     }
 };
